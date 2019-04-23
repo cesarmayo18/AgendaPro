d0c6cf0ae45cd9113ebe27d5093bb1eb1056f086
Cesar Mayo <ceadmama@gmail.com>
2019-04-23 00:00:25 -0400

    Forecast Api (Incomplete)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
     def self.recharge_balance
         User.update_all("balance = balance + 10000")
+        puts Forecast::forecast_api
     end
+
+    def self.forecast_api
+        url = 'http://api.tiempo.com/index.php?api_lang=es&localidad=18578&affiliate_id=59nzhollgr59'
+        response = RestClient.get(url)
+        forecast = Hash.from_xml(response)["report"]["location"]["var"][3]["data"]["forecast"]
+        forecast.each do |day|
+            return true if day["id"].to_i.between? 6, 10
+        end
+        return false
+    end 
 end

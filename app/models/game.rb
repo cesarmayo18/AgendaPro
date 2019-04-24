95f43ab6833a391d7a62bd9c320a97bb7c45b123
Cesar Mayo <ceadmama@gmail.com>
2019-04-24 17:20:23 -0400

    Create function Play of game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,10 @@
 class Game < ApplicationRecord
     has_many :bets
-    enum amount: { green: 0, red: 1, black: 2}
+    enum result: { green: 0, red: 1, black: 2}
+
+    def self.play
+        current_game = Game.create()
+        Bet.round(current_game)
+    end
+
 end

1bbcc658c7e4665f0250bdfd62b8055b5674e0d7
Cesar Mayo <ceadmama@gmail.com>
2019-04-24 17:21:01 -0400

    Calculate average betting user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,25 @@ class User < ApplicationRecord
     has_many :bets
     def self.recharge_balance
         User.update_all("balance = balance + 10000")
-        puts Forecast::forecast_api
+        $forecast = User.forecast_api
     end
 
     def self.forecast_api
         url = 'http://api.tiempo.com/index.php?api_lang=es&localidad=18578&affiliate_id=59nzhollgr59'
         response = RestClient.get(url)
         forecast = Hash.from_xml(response)["report"]["location"]["var"][3]["data"]["forecast"]
+        puts forecast
         forecast.each do |day|
             return true if day["id"].to_i.between? 6, 10
         end
         return false
     end 
+
+    def bet
+        return self.balance if self.balance<=1000
+        average_bet = if $forecast then rand(4..10) else rand(8..15) end
+        bet = self.balance * average_bet/100
+        self.balance -= bet
+        return bet
+    end
 end

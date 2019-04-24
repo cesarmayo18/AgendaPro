84703284399bf8f4cb13d42d59faf5b90e7c93ea
Cesar Mayo <ceadmama@gmail.com>
2019-04-24 17:20:43 -0400

    Create betting round

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,4 +1,12 @@
 class Bet < ApplicationRecord
-  belongs_to :users
-  belongs_to :games
+  belongs_to :user
+  belongs_to :game
+
+  def self.round(game)
+    User.all.map do |user|
+      bet = user.bet()
+      user.save
+      Bet.create(game:game,user:user,amount:bet) if bet > 0
+    end
+  end
 end
